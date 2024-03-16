07b319a3661e29fa4bedfc76b15d93f5d40e64fa
amandaay <amandaay.aysma@gmail.com>
2024-03-16 11:44:03 -0700

    updated starter code

--- NoSQLDatabase.cpp
+++ NoSQLDatabase.cpp
@@ -1,6 +1,10 @@
 // File: NoSQLDatabase.cpp
 
 #include "NoSQLDatabase.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 NoSQLDatabase::NoSQLDatabase(const std::string& dbName) : databaseName(dbName) {
     // Initialize other members
@@ -10,48 +14,42 @@ NoSQLDatabase::~NoSQLDatabase() {
     // Cleanup resources
 }
 
-bool NoSQLDatabase::openDatabase() {
-    // Open the database file
+void NoSQLDatabase::openOrCreateDatabase() {
+    // Open the database file or Create if db not exist
+
 }
 
-bool NoSQLDatabase::createDatabase() {
-    // Create a new database file
+void NoSQLDatabase::putDataIntoDatabase() {
+    // Insert data from OS file into NoSQL database
+
 }
 
-bool NoSQLDatabase::closeDatabase() {
-    // Close the database file
+void NoSQLDatabase::getDataFromDatabase() {
+    // Download data file from NoSQL database to OS directory
+
+}   
+
+void NoSQLDatabase::delFileFromDatabase() {
+    // Delete file from NoSQL database
+
 }
 
-bool NoSQLDatabase::deleteDatabase() {
-    // Delete the database file
+void NoSQLDatabase::listAllDataFromDatabase() {
+    // List all data files in NoSQL database
+
 }
 
-void NoSQLDatabase::listFiles() {
-    // List all files in the directory
+void NoSQLDatabase::findValueFromDatabase() {
+    // Find value using a given key
+
 }
 
-bool NoSQLDatabase::addFile(const std::string& filename, const std::string& filetype) {
-    // Add a new file to the directory
-}
+void NoSQLDatabase::killDatabase() {
+    // Delete the NoSQL database
 
-bool NoSQLDatabase::removeFile(const std::string& filename) {
-    // Remove a file from the directory
 }
-
-bool NoSQLDatabase::putData(const std::string& filename, const std::string& data) {
-    // Insert data into the database file
-}
-
-std::string NoSQLDatabase::getData(const std::string& filename) {
-    // Retrieve data from the database file
-}
-
-bool NoSQLDatabase::removeData(const std::string& filename) {
-    // Remove data from the database file
-}
-
-// Implement other member functions for index operations, free block management, and CLI
 
 void NoSQLDatabase::runCLI() {
     // Implement the command-line interface
+
 }
