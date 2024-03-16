4b8a759fdba8f010e2241577cf274062badec09a
amandaay <amandaay.aysma@gmail.com>
2024-03-16 11:50:41 -0700

    added main

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "NoSQLDatabase.h"
+
+int main() {
+    // Create an instance of NoSQLDatabase
+    NoSQLDatabase db("my_database");
+
+    // Run the command-line interface
+    db.runCLI();
+
+    return 0;
+}
