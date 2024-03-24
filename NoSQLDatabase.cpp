738ab7c784593ee1030e02f0476d3c5e9348dc53
amandaay <amandaay.aysma@gmail.com>
2024-03-24 13:13:17 -0700

    updated

--- NoSQLDatabase.cpp
+++ NoSQLDatabase.cpp
@@ -266,8 +266,7 @@ void NoSQLDatabase::putDataIntoDatabase(string &myFile)
         // last modified time
         databaseFile.seekp((i + 1) * (BLOCK_SIZE + 1) + 60);
         localTime = localtime(&directory[i].timestamp);
-        // Adjust local time for GMT-7 (PST with DST)
-        // localTime->tm_hour -= 7;
+        
         // Ensure hour is within 0-23 range
         if (localTime->tm_hour < 0)
         {
